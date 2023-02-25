f40f785beaaee2d69671d9dec182a81947f476bd
skeletus-design <nikita.voro24nin@gmail.com>
2023-02-25 14:55:37 +0300

    3

---
+++ Phone/src/voronin/phone.java
@@ -0,0 +1,59 @@
+package voronin;
+
+import org.omg.CORBA.portable.InputStream;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class phone {
+
+    private final Socket socket;
+    private final BufferedReader reader;
+    private final BufferedWriter writer;
+    public phone(String ip, int port) {
+        try {
+            this.socket = new Socket(ip, port);
+            this.reader = createReader();
+            this.writer = createWriter();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public phone(ServerSocket server) {
+        try {
+            this.socket = server.accept();
+            this.reader = createReader();
+            this.writer = createWriter();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void writeLine (String message) {
+        try {
+            writer.write(message);
+            writer.newLine();
+            writer.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String readLine() {
+        try {
+            return reader.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private BufferedReader createReader() throws IOException {
+            return new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    }
+    private BufferedWriter createWriter() throws IOException {
+            return new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+    }
+}
